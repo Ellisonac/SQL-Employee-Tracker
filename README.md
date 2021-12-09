e73afe3ffd68fbb6cd1d8e663eebedea6ffc58c4
Andrew Ellison <acannonellison@gmail.com>
2021-12-08 21:28:17 -0800

    Added a basic README file.

---
+++ README.md
@@ -0,0 +1,70 @@
+# Employee Database CLI
+
+## Description
+
+This command line interface allows users to view and edit employee data stored in a local MySQL database. This application is based in Node.js and uses the Inquirer and MySQL2 node packages.
+
+See a walkthrough video at: [Walkthrough]()
+
+![License Badge](https://img.shields.io/badge/License-MIT-informational?logoColor=white&color=1CA2F1)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [How to Use](#how-to-use)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+
+This project can be cloned from this database and used with a local MySQL server.
+
+## How to Use
+
+To start using the employee database, a user can run the schema and seeds sql file with
+
+```console
+  mysql -u root -p < db/schema.sql < db/seeds.sql
+```
+
+Then the application can be run from the command line with
+
+```console
+  node server.js
+```
+
+The interface will then prompt the user to select from view, create, and update operations.
+
+## Questions
+
+Find my other projects at: [ellisonac](https://github.com/ellisonac)
+
+Contact me at: acannonellison@gmail.com
+
+## License
+
+![License Badge](https://img.shields.io/badge/License-MIT-informational?logoColor=white&color=1CA2F1)
+
+This project is covered under the following license:
+
+MIT License
+
+Copyright (c) [2021]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- utils/updateEntries.js
+++ utils/updateEntries.js
@@ -2,47 +2,36 @@ const inquirer = require("inquirer");
 
 // Allow users to update an employee entries name, role, and manager
 const updateEmployee = async (state) => {
+  // Get a valid employeeID from user input
+  const employee = await getEmployeeID(state);
+
   const questions = [
     {
       type: "input",
-      name: "id",
-      message: "Select an employee ID",
-    },
-    {
-      type: "input",
       name: "first",
-      message: "Enter the employee's new first name or blank to skip",
+      message: `Enter the ${employee.first_name}'s new first name or blank to skip`,
     },
     {
       type: "input",
       name: "last",
-      message: "Enter the employee's new last name or blank to skip",
+      message: `Enter the ${employee.first_name}'s new last name or blank to skip`,
     },
     {
       type: "list",
       name: "title",
-      message: "Enter the employee's new role",
+      message: `Enter the ${employee.first_name}'s new role`,
       choices: state.rolesState.map((role) => role.title),
     },
     {
       type: "list",
       name: "manager",
-      message: "Select the employee's new manager",
+      message: `Select the ${employee.first_name}'s new manager`,
       choices: state.managersState.map((manager) => manager.name),
     },
   ];
 
+  //Prompt user to update pulled employee
   const response = await inquirer.prompt(questions);
-
-  if (!response.id) return;
-
-  const employeeQuery = await state.db.query(
-    `SELECT * FROM employees WHERE id = ${response.id}`
-  );
-
-  const employee = employeeQuery[0][0];
-
-  console.log(employee);
 
   if (employee) {
     const role_id = state.rolesState.filter(
@@ -55,10 +44,10 @@ const updateEmployee = async (state) => {
 
     const insertQuery = `UPDATE employees SET 
     first_name = '${response.first || employee.first_name}',
-    last_name = '${response.first || employee.last_name}',
+    last_name = '${response.last || employee.last_name}',
     manager_id = ${manager_id},
     role_id = '${role_id || employee.role_id}'
-    WHERE id = '${response.id}';`;
+    WHERE id = '${employee.id}';`;
 
     await state.db.query(insertQuery);
   } else {
@@ -66,5 +55,33 @@ const updateEmployee = async (state) => {
   }
   return;
 };
+
+const getEmployeeID = async (state) => {
+  let isValid;
+  let employee;
+  while (!isValid) {
+    const idResponse = await inquirer.prompt([
+      {
+      type: "input",
+      name: "id",
+      message: "Select an employee ID",
+      },
+    ]);
+
+    try {
+      const employeeQuery = await state.db.query(
+        `SELECT * FROM employees WHERE id = ${idResponse.id}`
+      );
+      employee = employeeQuery[0][0];
+
+      isValid = true
+    } catch (err) {
+      isValid = false;
+      console.log('Please enter a valid employee ID');
+    }
+  }
+
+  return employee;
+}
 
 module.exports = { updateEmployee };
